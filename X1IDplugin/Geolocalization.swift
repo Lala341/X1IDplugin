91685c0ae6ed9e9b4613c63d6a2db7ec0f50ec8f
IsaLegal <isabella@somoslegal.com>
2020-11-24 16:22:09 -0500

    Cambios

--- X1IDplugin/Geolocalization.swift
+++ X1IDplugin/Geolocalization.swift
@@ -6,10 +6,52 @@
 //
 
 import Foundation
+import UIKit
+import CoreLocation
 
-public class Geolocalization{
+public class Geolocalization:  CLLocationManagerDelegate{
     public init(){}
-    public func verififyPermissions(){}
+    public func verifyPermissions(){
+        
+        if CLLocationManager.locationServicesEnabled() {
+    switch CLLocationManager.authorizationStatus() {
+        case .notDetermined, .restricted, .denied:
+            return false
+        case .authorizedAlways, .authorizedWhenInUse:
+            return true
+        @unknown default:
+            return false
+        break
+    }
+    } else {
+        print("Location services are not enabled")
+        return false
+}
+    }
+    public func requestPermissions(){
+        var locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+      
+    }
+
+public func getLocation(){
+        var locationManager = CLLocationManager()
+if CLLocationManager.locationServicesEnabled() {
+    locationManager.delegate = self
+    locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+    locationManager.startUpdatingLocation()
+}
+      
+    }
+
     
+func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+    print("locations = \(locValue.latitude) \(locValue.longitude)")
+}
+
 
 }
--- X1IDplugin/VideoStreamming.swift
+++ X1IDplugin/VideoStreamming.swift
@@ -11,5 +11,27 @@ import Foundation
 
 public class VideoStreamming{
     public init(){}
+    public func verifyPermissions(){
+        
+        if AVCaptureDevice.authorizationStatus(for: .video) ==  .authorized {
+    return true
+} else {
+    AVCaptureDevice.requestAccess(for: .video, completionHandler: { (granted: Bool) in
+        if granted {
+           return true
+        } else {
+            return false
+        }
+    })
+}
+    }
+    public func requestPermissions(){
+        var locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+      
+    }
 
 }
